ca735e82e4998eedc40d4877b981fc344fd66e1c
jgru <jang92@gmx.de>
2019-05-05 11:13:26 +0200

    Adds jupyter notebook

--- reg_parser.py
+++ reg_parser.py
@@ -1,3 +1,5 @@
+#!/opt/conda/bin/python3
+
 __author__ = 'gru'
 
 import logging
@@ -12,48 +14,78 @@ def init_logging():
     logging.info("Logging set up completed")
 
 
+def str_sep():
+    s = "\n\n\n"
+    s += "===" * 10
+    return s
+
+
 if __name__ == "__main__":
-    init_logging()
+
     ap = argparse.ArgumentParser(prog='reg-parser', description='A command '
                                                                'line tool to extract registry keys', epilog="Maintained by _|.62|_|")
+    # Define positional argument
+    ap.add_argument("input", help="path to hive (e.g. ~/tmp/NTUser.dat)")
 
     # Define optional arguments
-
     ap.add_argument("-r", "--raw", action="store_true", help="Print raw "
                                                              "values")
     ap.add_argument("-b", "--bare", action="store_true", help="Print bare "
-                                                            "values (e.g. "
+                                                            "content (e.g. "
                                                             "for filtering "
                                                             "in XWays)")
     ap.add_argument("-t", "--treeStructure", action="store_true",
-                    help="Print all keys")
+                    help="Print all keys until specified depth")
+
     ap.add_argument("-o", "--output", nargs="?", help="Write result to file")
 
     ap.add_argument("-s", "--searchKeys", nargs="?",
-                    help="Keys to search, maybe separated by commata without "
+                    help="Keys to search, may be separated by commata without "
                          "whitespace")
 
-    # Define positional argument
-    ap.add_argument("input", help="path to hive (e.g. ~/tmp/NTUser.dat")
+    ap.add_argument("-f", "--searchFragments", nargs="?",
+                    help="Fragment/part of key to search, may be separated by commata without "
+                         "whitespace")
+
     args = ap.parse_args()
 
+    init_logging()
     # Define and init result
     result = ""
 
     # Create RegistryManager
     hive = HiveManager(args.input)
-
+    result = ""
     if args.treeStructure:
-        result = hive.get_tree_structure(hive.root, print_branch=True)
-    print(result)
+        tree = hive.get_tree_structure(hive.root, print_branch=True)
+        result += "[*] Hive tree"
+        result += tree
+        result += str_sep()
+
     # Process search keys
-    sk = args.searchKeys.split(",")
-    print("Keys to search: ", sk)
-    if sk is not None:
-        for s in sk:
-            # Retrieve RecentDocs
-            result += str(hive.retrieve(s, args.bare, args.raw))
+    if args.searchKeys is not None:
+        sk = args.searchKeys.split(",")
+        result += "\n[*] Keys to search: " + str(sk) + "\n"
+
+        if sk is not None:
+            for s in sk:
+                # Retrieve RecentDocs
+                result += str(hive.retrieve(s, args.bare, args.raw))
+
+        result += str_sep()
+
+    # Process search fragments
+    if args.searchFragments is not None:
+        sf = args.searchFragments.split(",")
+        result += "\n[*] Key fragments to search: " + str(sf) + "\n"
+
+        if sf is not None:
+            for s in sf:
+                # Search for fragments
+                result += str(hive.search_key([hive.root], s))
+
     print(result)
+
     if args.output is not None:
         print("Write to: " + args.output)
         with open(args.output, "w+") as f:
